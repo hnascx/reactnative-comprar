258acb2fcf4d5a4c6c5e0f959696a7fc823cd2b6
hnascx <henrique.nascimento12@hotmail.com>
2025-10-15 14:54:32 -0300

    feat: Create image component

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    height: 34,
+    width: 134,
+  },
+})
