6d46471e8330560a9ef7a0def1d6437fdd635bd2
hnascx <henrique.nascimento12@hotmail.com>
2025-10-15 15:24:51 -0300

    feat: Create button component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,13 @@
+import { Button } from "@/components/Button"
 import { Image, View } from "react-native"
 import { styles } from "./styles"
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image source={require("../../assets/logo.png")} style={styles.logo} />
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <Button />
     </View>
   )
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -2,10 +2,11 @@ import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#000",
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#D0D2D8",
+    paddingTop: 62,
+    padding: 24,
   },
   logo: {
     height: 34,
