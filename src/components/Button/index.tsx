3d91441fc0dc16d16ea7f6b565a5a62e2e540839
hnascx <henrique.nascimento12@hotmail.com>
2025-10-20 10:47:10 -0300

    feat: Using props on component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,15 @@
-import { Text, TouchableOpacity } from "react-native"
+import { Text, TouchableOpacity, TouchableOpacityProps } from "react-native"
 
 import { styles } from "./styles"
 
-export function Button() {
+interface ButtonProps extends TouchableOpacityProps {
+  title: string
+}
+
+export function Button({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.8}>
-      <Text style={styles.title}>Adicionar</Text>
+    <TouchableOpacity style={styles.container} activeOpacity={0.8} {...rest}>
+      <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   )
 }
