75be993e1bb4b5fa07e18ddbff0491a5863a05bf
hnascx <henrique.nascimento12@hotmail.com>
2025-10-20 11:20:09 -0300

    feat: Create content container

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -8,8 +8,14 @@ export function Home() {
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <Input placeholder="O que você deseja comprar?" />
-      <Button title="Adicionar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você deseja comprar?" />
+        <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}>
+
+      </View>
     </View>
   )
 }
